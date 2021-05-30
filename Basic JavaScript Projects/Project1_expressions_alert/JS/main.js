80240a5408d0e8dc9cb11e235288e484975bcd3b
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2021-05-29 20:24:20 -0400

    First Basic JS project commit
    introductory JS exercises for the Tech Academy

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,28 @@
+window.alert("Hello, World!"); //window
+
+document.write('Hello, world!'); //write on page
+
+var x = " String VALUE!"; //assigning a variable x
+
+document.write (x); //writing variable x
+
+var y = " More Strings"; // assign variable y
+
+window.alert (y); //window for variable y
+
+document.write ("He told her, \"You\'ll be a great public speaker.\"<br>\"Maybe,\" she replied."); //escaping practice
+
+document.write (" Hello," + " World!"); //concatenating
+
+var b = "concatenated" + " string" //concatenating in a variable &writing it
+document.write (b)
+
+var animals = "cats", orange = "Momo", gray = "Martha", spotted = "Toph"; // assigning multiple variables in single statment
+var animals = animals.fontcolor ("red");
+var orange  = orange.fontcolor ("orange");
+var gray = gray.fontcolor ("purple");
+var spotted = spotted.fontcolor ("blue");
+
+document.write (orange);
+
+document.write (3 * 3); //writing an expression
