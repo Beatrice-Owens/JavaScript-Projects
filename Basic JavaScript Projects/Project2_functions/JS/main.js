a3399853dcf4c40f24af462419a189eed29355fa
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2021-06-01 18:13:37 -0400

    project 2 commit
    html and js push  for project2 for the Tech Academy

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+//create function to designate two variables and use getElementById  method
+function displayCat() {
+    var catOne = "Momo"; 
+    var catTwo = "Martha";
+    document.getElementById("catName").innerHTML = catOne;
+}
+
+//create function to concatenate a string variable
+function learnFunction() {
+    var sentence = "I was learning";
+    sentence += " before I read this book";
+    document.getElementById("conc").innerHTML = sentence;
+}
