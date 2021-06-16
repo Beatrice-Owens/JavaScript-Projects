f22f7bf08e0d571dc17d8e73c687c3a95fd9775a
Beatrice-Owens <83464212+Beatrice-Owens@users.noreply.github.com>
2021-06-16 12:54:13 -0400

    Project10 commit
    loops and arrays assignments for The Tech Academy

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,56 @@
+//while loop
+function Call_Loop() {
+    var count = "";
+    var x = 1;
+    while (x < 11) {
+        count += "<br>" + x;
+        x++;
+    }
+    document.getElementById("Loop").innerHTML = count;
+}
+
+//for loop
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y; 
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+//Array
+function array_Function() {
+    var animals = [];
+    animals[0] = "cat";
+    animals[1] = "dog";
+    animals[2] = "bird";
+    animals[3] = "fish"; 
+    document.getElementById("Array").innerHTML = "This animal is a " + 
+        animals[2] + ".";
+}
+
+//Constant
+function constant_function() {
+    const animal = {type:"cat", name:"Momo", age:"3"};
+    animal.age = "4";
+    animal.color = "orange";
+    document.getElementById("Constant").innerHTML = animal.name + " is " + animal.color + ".";
+}
+
+function let_function() {
+    let a = 9;
+    document.getElementById("Lets").innerHTML = a;
+}
+
+let animal = {
+    type: "cat ", 
+    name: "Momo ",
+    age: "4 ", 
+    color: "orange ", 
+    description : function() {
+        return this.name + "is a " + this.age + "year old " + this.color + this.type + ".";
+    }
+};
+document.getElementById("animal_Object").innerHTML = animal.description();
